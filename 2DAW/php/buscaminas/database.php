2494370c98c5e7b7c1eb388f9ef3b8470643b387
XxSergioLDCxX <119493186+XxSergioLDCxX@users.noreply.github.com>
2023-10-16 10:49:31 +0200

    a

--- 2DAW/php/buscaminas/database.php
+++ 2DAW/php/buscaminas/database.php
@@ -17,9 +17,41 @@
 
       /******************** Cerrar la conexión *********************/
       mysqli_close($conexion);
-      print "Conexión 2 cerrada" . "<br>";
 
 
+        }
+
+        function agregarUsuario($conexion, $usuario,$pass,$correo,$ad = false){
+          $sql = "INSERT INTO usuarios (nombre_usuario, contrasena, correo, administrador) VALUES ('$usuario', '$pass', '$correo', '$ad')";
+          if ($conexion->query($sql) === TRUE) {
+            echo "Usuario $usuario agregado con éxito.<br>";
+        } else {
+            echo "Error al agregar el usuario $usuario: " . $conexion->error . "<br>";
+        }
+        }
+
+        function loggin($conexion,$usuario,$contrasena){
+           
+          
+          //MIRAR NOMBRE DE USUARIO Y CONTRASEÑA
+        $sql = "SELECT nombre_usuario, contrasena FROM usuarios WHERE nombre_usuario = '$usuario'";
+        $result = $conexion->query($sql);
+          if ($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $contrasena_almacenada = $row["contrasena"];
+        
+            // Verificar si la contraseña ingresada coincide con la contraseña almacenada
+            if ($contrasena === $contrasena_almacenada) {
+                
+                return true;
+            } else {
+          
+                return false;
+            }
+        } else {
+            
+            return false;
+        }
         }
 
           
--- 2DAW/php/buscaminas/index.php
+++ 2DAW/php/buscaminas/index.php
@@ -1,13 +1,74 @@
 <?php
  require_once 'database.php';
 
+ header("Content-Type:application/json");
+ $requestMethod = $_SERVER["REQUEST_METHOD"];
+
  //Variables para la base de datos 
  $sv = 'localhost';
  $user = 'root';
  $pass = '';
- $dtb = 'BuscaMinas';
+ $dtb = 'buscaminas';
 
  //Conexion a la base de datos
  $conexion = conectarBaseDatos($sv, $user, $pass, $dtb);
+       
+ 
+        $usuario = "Sergio";
+        $contrasena = "add123";
+
+       $auth = loggin($conexion,$usuario,$contrasena);
+
+       if ($auth == true) {
+        
+        switch ($requestMethod) {
+            case "POST":
+                    
+        
+        
+                $response = array("mensaje" => "Solicitud POST recibida.");
+        
+                break;
+            case "GET":
+                //crear el tablero si no existe
+
+        
+                $response = array("mensaje" => "Solicitud GET recibida.");
+                break;
+        
+            case "PUT":
+        
+                $response = array("mensaje" => "Solicitud PUT recibida.");
+        
+                break;
+            default:
+        
+                header("HTTP/1.1 405 Verbo no soportado");
+                $response = array("error" => "Método HTTP no válido.");
+        }
+
+
+       }else{
+
+        $response = array("mensaje" => "Usuario o contraseña incorrecta.");
+
+       }
+
+
+echo json_encode($response);
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
 
  $conexion = desconectarBaseDatos($conexion);
